91687ef7da5990c27669c1936a2b4d7372b4fcc7
Mighty Blue <mightyblue01@users.noreply.github.com>
2017-04-24 20:36:12 +1000

    Add files via upload
    Added new post code range.

--- Code/RCode/MedicationByPostCode.R
+++ Code/RCode/MedicationByPostCode.R
@@ -4,29 +4,35 @@ library("dplyr")
 # pass the MySQL Credentials - 
 mydb = dbConnect(MySQL(), user='xxxx', password='password', dbname='MelbDatathon2017', host='localhost')
 
-
-
-
-
 zip_param <- 3000
 
-result = data.frame()
-for(zip_param in 3000:3139) {
+resultSet = data.frame()
+for(zip_param in 3000:3999) {
   pull2 <- dbSendQuery(mydb, paste("select postcode, ATCLevel5Name, count(ATCLevel5Name) as consumption from ZipCodeATC5View
                                 where postcode= 
                                    ",zip_param,"group by ATCLevel5Name
                                    order by count(ATCLevel5Name) desc limit 3;"))
                       
-                      data3 = fetch(pull2, n=-1)
+                      data = fetch(pull2, n=-1)
                       #print(data3)
-                      result <- rbind( result, data.frame(data3))
-                      
-                      
-}
-print(result)
-write.csv(result, "//Volumes//JetDrive//MedicationByLocation.csv",row.names=FALSE) 
+                      resultSet <- rbind(resultSet, data.frame(data))
 
-#rm(result)
+}
+
+for(zip_param in 8000:8999) {
+  pull2 <- dbSendQuery(mydb, paste("select postcode, ATCLevel5Name, count(ATCLevel5Name) as consumption from ZipCodeATC5View
+                                   where postcode= 
+                                   ",zip_param,"group by ATCLevel5Name
+                                   order by count(ATCLevel5Name) desc limit 3;"))
+  
+  data = fetch(pull2, n=-1)
+  #print(data3)
+  resultSet <- rbind(resultSet, data.frame(data))
+}
+print(resultSet)
+write.csv(resultSet, "//Volumes//JetDrive//MedicationByLocationFile1.csv",row.names=FALSE) 
+
+#rm(resultSet)
 #lapply( dbListConnections( dbDriver( drv = "MySQL")), dbDisconnect)
 
 
